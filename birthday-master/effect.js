e34d05abb848f7086931ebf0df592eb74328521d
ramrajep <154545752+ramrajep@users.noreply.github.com>
2023-12-26 10:05:06 +0530

    Update effect.js

--- birthday-master/effect.js
+++ birthday-master/effect.js
@@ -176,8 +176,8 @@ $('document').ready(function(){
 			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
 			i=i+1;
 			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(58)").fadeOut('slow').promise().done(function () {
+			if(i==53){
+				$("p:nth-child(52)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
 				});
 				
